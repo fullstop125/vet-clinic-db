8fe8d35d28cf2433b8bf984f1834e346ba1b7f14
fullstop125 <hassanmoses125@gmail.com>
2022-10-19 01:09:10 +0300

    add queries to the database

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
+drop TABLE if exists animals;
+CREATE TABLE animals(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts NUMERIC NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL
 );
