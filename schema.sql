0fe67aa6469a9022a54d189d3ec4e3984160e937
fullstop125 <hassanmoses125@gmail.com>
2022-10-20 23:41:30 +0300

    query multiple tables

--- schema.sql
+++ schema.sql
@@ -1,12 +1,40 @@
 /* Database schema to keep the structure of entire database. */
-drop TABLE if exists animals;
-CREATE TABLE animals(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    escape_attempts NUMERIC NOT NULL,
-    neutered BOOLEAN NOT NULL,
-    weight_kg DECIMAL NOT NULL
-    
+
+
+
+drop TABLE if exists owners CASCADE;
+drop TABLE if exists species CASCADE;
+
+CREATE TABLE owners (
+    id bigserial,
+    full_name varchar(100) NOT NULL,
+    age INT,
+    PRIMARY KEY (id)
 );
- ALTER TABLE animals ADD COLUMN species VARCHAR(100);
+
+CREATE TABLE species (
+    id bigserial,
+    name varchar(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+drop TABLE if exists animals;
+
+
+CREATE TABLE animals (
+    id bigserial,
+    name varchar(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL NOT NULL,
+    PRIMARY KEY (id),
+    species_id INT,
+    owners_id INT,
+    CONSTRAINT species
+     FOREIGN KEY (species_id)
+      REFERENCES species(id),
+       CONSTRAINT owners
+     FOREIGN KEY (owners_id)
+      REFERENCES owners(id)
+);
